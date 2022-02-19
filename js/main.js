49c3b94b21e55645093db28c010f1fe52c1a8bb4
svoboda-core <vasilykonoval@gmail.com>
2022-02-20 00:59:47 +0200

    feat: fin section desing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+	$('.desing-slider').slick({
+		dots: false,
+		slidesToShow: 4,
+		variableWidth: true,
+		prevArrow: '<img class="arrow arrow-left" src="images/arrow-2.svg" alt="">',
+		nextArrow: '<img class="arrow arrow-right" src="images/arrow-1.svg" alt="">'
+	});
 });
