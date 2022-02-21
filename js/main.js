163438abf45faa54efc974dfccd6ab9f6aed4e81
svoboda-core <vasilykonoval@gmail.com>
2022-02-21 15:56:17 +0200

    feat: add responsive design

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(function(){
 		slidesToShow: 4,
 		variableWidth: true,
 		prevArrow: '<img class="arrow arrow-left" src="images/arrow-2.svg" alt="">',
-		nextArrow: '<img class="arrow arrow-right" src="images/arrow-1.svg" alt="">'
+		nextArrow: '<img class="arrow arrow-right" src="images/arrow-1.svg" alt="">',
+		responsive: [
+			{
+				breakpoint: 361,
+				settings: {
+					slidesToShow: 1,
+					variableWidth: false,
+				
+				}
+			},]
 	});
 });
